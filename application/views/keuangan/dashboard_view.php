032d04cb472fecbba45a708d9322741db1a70ad9
yeunarnd <ynarnd23@gmail.com>
2021-12-19 22:27:15 +0700

    ap

--- application/views/keuangan/dashboard_view.php
+++ application/views/keuangan/dashboard_view.php
@@ -19,11 +19,7 @@
           <span>Utama</span>
         </a>
       </li>
-      <li class="nav-item <?php echo $this->uri->segment(2) == 'home' ? 'active' : '' ?>">
-        <a class="nav-link" href="<?php echo site_url('pengguna') ?>">
-          <img src="<?php echo base_url() ?>assets/img/data pengguna.png" width="18" height="18">
-          <span>Data Pengguna</span></a>
-      </li>
+
       <li class="nav-item dropdown <?php echo $this->uri->segment(2) == 'home' ? 'active' : '' ?>">
         <a class="nav-link dropdown-toggle" href="#" id="pagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <img src="<?php echo base_url() ?>assets/img/rincian.png" width="20" height="20">
@@ -41,7 +37,7 @@
           <span>Perancangan</span>
         </a>
         <div class="dropdown-menu" aria-labelledby="pagesDropdown">
-          <a class="dropdown-item" href="<?php echo site_url('home/add') ?>">Tambah Proyek</a>
+          <a class="dropdown-item" href="<?php echo site_url('proyek/add') ?>">Tambah Proyek</a>
           <a class="dropdown-item" href="<?php echo site_url('home') ?>">Data Pengeluaran</a>
           <a class="dropdown-item" href="<?php echo site_url('home') ?>">Hitung RAB</a>
         </div>
