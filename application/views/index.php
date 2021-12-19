643db50a2951dd3afdd080e2968015da22e22403
Nadhief Fawaz Rezaka <ndhfwzproject@gmail.com>
2021-12-19 21:48:39 +0700

    haiii

--- application/views/index.php
+++ application/views/index.php
@@ -54,7 +54,7 @@
         <!-- Menu For Mobile Device -->
         <div class="mobile-nav">
             <a href="index.html" class="logo">
-                <h5>WINA CINEMA FILM</h5>
+                <h5>DE' DIAMOND PARK</h5>
                 <!-- <img src="assets/img/logo.png" alt="Logo"> -->
             </a>
         </div>
@@ -71,150 +71,15 @@
                     <div class="collapse navbar-collapse mean-menu">
                         <ul class="navbar-nav m-auto">
                             <li class="nav-item">
-                                <a href="#" class="nav-link active">
-                                    Home
-                                    <i class="bx bx-chevron-down"></i>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item">
-                                        <a href="index.html" class="nav-link active">Home One</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="index-2.html" class="nav-link">Home Two</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="index-3.html" class="nav-link">Home Three</a>
-                                    </li>
-                                </ul>
+                                <a href="#" class="nav-link active">Beranda</a>
                             </li>
 
                             <li class="nav-item">
-                                <a href="about.html" class="nav-link">About</a>
+                                <a href="about.html" class="nav-link">Tentang Kami</a>
                             </li>
 
                             <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    Pages
-                                    <i class="bx bx-chevron-down"></i>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item">
-                                        <a href="testimonials.html" class="nav-link">Testimonials</a>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a href="#" class="nav-link">
-                                            Portfolio
-                                            <i class='bx bx-chevron-right'></i>
-                                        </a>
-
-                                        <ul class="dropdown-menu">
-                                            <li class="nav-item">
-                                                <a href="portfolio-columns-two.html" class="nav-link">Portfolio Columns Two</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="portfolio-columns-three.html" class="nav-link">Portfolio Columns Three</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="portfolio-columns-four.html" class="nav-link">Portfolio Columns Four</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="portfolio-details.html" class="nav-link">Portfolio Details</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a href="team.html" class="nav-link">Team</a>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a href="#" class="nav-link">
-                                            User
-                                            <i class='bx bx-chevron-right'></i>
-                                        </a>
-
-                                        <ul class="dropdown-menu">
-                                            <li class="nav-item">
-                                                <a href="my-account.html" class="nav-link">My Account</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="log-in.html" class="nav-link">Log In</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="sign-in.html" class="nav-link">Sign In</a>
-                                            </li>
-                                            <li class="nav-item">
-                                                <a href="recover-password.html" class="nav-link">Recover Password</a>
-                                            </li>
-                                        </ul>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a href="faq.html" class="nav-link">FAQ</a>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a href="coming-soon.html" class="nav-link">Coming Soon</a>
-                                    </li>
-
-                                    <li class="nav-item">
-                                        <a href="404.html" class="nav-link">404 Error Page</a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    Shop
-                                    <i class="bx bx-chevron-down"></i>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item">
-                                        <a href="shop.html" class="nav-link">Shop</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="cart.html" class="nav-link">Cart</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="checkout.html" class="nav-link">Checkout</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="product-details.html" class="nav-link">Product Details</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="wishlist.html" class="nav-link">Wishlist</a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li class="nav-item">
-                                <a href="#" class="nav-link">
-                                    Blog
-                                    <i class="bx bx-chevron-down"></i>
-                                </a>
-
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item">
-                                        <a href="blog-grid.html" class="nav-link">Blog Grid</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="blog-left-sidebar.html" class="nav-link">Blog Left Sidebar</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="blog-right-sidebar.html" class="nav-link">Blog Right Sidebar</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a href="blog-details.html" class="nav-link">Blog Details</a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li class="nav-item">
-                                <a href="contact.html" class="nav-link">Contact</a>
+                                <a href="contact.html" class="nav-link">Kontak</a>
                             </li>
 
                             <li class="nav-item">
@@ -1278,7 +1143,7 @@
             <div class="row">
                 <div class="col-lg-3 col-md-6">
                     <div class="single-widget">
-                        <img src="<?= base_url('front-end/'); ?>assets/img/footer-logo.png" alt="Image">
+                        <img src="<?= base_url('front-end/assets/img/logo.png'); ?>">
 
                         <div class="newsletter-wrap">
                             <h3>Subscribe Newsletter</h3>
