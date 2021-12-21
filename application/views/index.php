ea499649da0fdc5d689653d9612faafc094a1c22
Nadhief Fawaz Rezaka <ndhfwzproject@gmail.com>
2021-12-21 21:04:24 +0700

    `

--- application/views/index.php
+++ application/views/index.php
@@ -1021,7 +1021,12 @@
                     </div>
                 </div>
 
-                <iframe src=" https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.262469571367!2d112.51144501474558!3d-7.324388094714075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78079b68ad50f5%3A0x594a447648a44277!2sDE&#39;%20DIAMOND%20PARK%20KEDAMEAN!5e0!3m2!1sen!2sid!4v1640092449221!5m2!1sen!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                <div class="col-lg-3 col-md-6">
+                    <div class="single-widget">
+                        <iframe src=" https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3957.262469571367!2d112.51144501474558!3d-7.324388094714075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e78079b68ad50f5%3A0x594a447648a44277!2sDE&#39;%20DIAMOND%20PARK%20KEDAMEAN!5e0!3m2!1sen!2sid!4v1640092449221!5m2!1sen!2sid" 
+                        width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                    </div>
+                </div>
                 <!-- <div class="col-lg-3 col-md-6">
                     <div class="single-widget">
                         <h3>Additional Links</h3>
