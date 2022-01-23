5bcde305c94e99f881caa8d0f32433a3aaa4dd6b
Nadhief Fawaz Rezaka <ndhfwzproject@gmail.com>
2022-01-23 23:09:31 +0700

    newww

--- application/views/proyek/edit_form.php
+++ application/views/proyek/edit_form.php
@@ -3,7 +3,7 @@
 
 <head>
 	<?php $this->load->view("_partials/head.php") ?>
-	<title> Tambah Proyek - DeDiamondPark </title>
+	<title> Edit Proyek - DeDiamondPark </title>
 </head>
 
 <body id="page-top">
@@ -25,16 +25,22 @@
 					</div>
 				<?php endif; ?>
 
+				<!-- Card  -->
 				<div class="card mb-3">
 					<div class="card-header">
-						<a href="<?php echo site_url('proyek') ?>"><i class="fas fa-arrow-left"></i> Kembali</a>
+
+						<a href="<?php echo site_url('proyek') ?>"><i class="fas fa-arrow-left"></i>
+							Kembali</a>
 					</div>
 					<div class="card-body">
 
-						<form action="<?php base_url('proyek/add') ?>" method="post" enctype="multipart/form-data">
+						<form action="<?php base_url("proyek/edit") ?>" method="post" enctype="multipart/form-data">
+
+							<input type="hidden" name="id" value="<?php echo $proyek->kd_proyek ?>" />
+
 							<div class="form-group">
 								<label for="kd_proyek">Kode Proyek</label>
-								<input class="form-control <?php echo form_error('kd_proyek') ? 'is-invalid' : '' ?>" type="text" name="kd_proyek" placeholder="Kode Proyek" />
+								<input class="form-control <?php echo form_error('kd_proyek') ? 'is-invalid' : '' ?>" type="text" name="kd_proyek" placeholder="Kode Proyek" value="<?php echo $proyek->kd_proyek ?>" />
 								<div class="invalid-feedback">
 									<?php echo form_error('kd_proyek') ?>
 								</div>
@@ -42,7 +48,7 @@
 
 							<div class="form-group">
 								<label for="nama_proyek">Nama Proyek</label>
-								<input class="form-control <?php echo form_error('nama_proyek') ? 'is-invalid' : '' ?>" type="text" name="nama_proyek" placeholder="Nama Proyek" />
+								<input class="form-control <?php echo form_error('nama_proyek') ? 'is-invalid' : '' ?>" type="text" name="nama_proyek" min="0" placeholder="Nama Proyek" value="<?php echo $proyek->nama_proyek ?>" />
 								<div class="invalid-feedback">
 									<?php echo form_error('nama_proyek') ?>
 								</div>
@@ -50,7 +56,7 @@
 
 							<div class="form-group">
 								<label for="type">Type</label>
-								<input class="form-control <?php echo form_error('type') ? 'is-invalid' : '' ?>" type="text" name="type" placeholder="Type" />
+								<input class="form-control <?php echo form_error('type') ? 'is-invalid' : '' ?>" type="text" name="type" min="0" placeholder="Type" value="<?php echo $proyek->type ?>" />
 								<div class="invalid-feedback">
 									<?php echo form_error('type') ?>
 								</div>
@@ -58,19 +64,11 @@
 
 							<div class="form-group">
 								<label for="pemilik_proyek">Pemilik Proyek</label>
-								<input class="form-control <?php echo form_error('pemilik_proyek') ? 'is-invalid' : '' ?>" type="text" name="pemilik_proyek" placeholder="Pemilik Proyek" />
+								<input class="form-control <?php echo form_error('pemilik_proyek') ? 'is-invalid' : '' ?>" type="text" name="pemilik_proyek" min="0" placeholder="pemilik_proyek" value="<?php echo $proyek->pemilik_proyek ?>" />
 								<div class="invalid-feedback">
 									<?php echo form_error('pemilik_proyek') ?>
 								</div>
 							</div>
-
-							<!-- <div class="form-group">
-								<label for="keterangan_lain">Keterangan Lain</label>
-								<input class="form-control <?php echo form_error('keterangan_lain') ? 'is-invalid' : '' ?>" type="text" name="keterangan_lain" placeholder="Keterangan Lain" />
-								<div class="invalid-feedback">
-									<?php echo form_error('keterangan_lain') ?>
-								</div>
-							</div> -->
 
 							<input class="btn btn-dark" type="submit" name="btn" value="Simpan" />
 						</form>
@@ -93,7 +91,6 @@
 
 		</div>
 		<!-- /#wrapper -->
-
 
 		<?php $this->load->view("_partials/scrolltop.php") ?>
 
