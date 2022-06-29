79fdd5813d077a4fdc75a3dd3ff433b5327653d4
yeunarnd <ynarnd23@gmail.com>
2022-06-29 22:45:51 +0700

    ctrl

--- application/controllers/Bahan.php
+++ application/controllers/Bahan.php
@@ -60,19 +60,30 @@ class Bahan extends CI_Controller
     {
         if (!isset($id)) redirect('bahan');
 
+        $data['title'] = 'Edit Daftar Rincian Bahan';
+        $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
+
+        $where = array('id' => $id);
+        $data['bahan'] = $this->bahan_model->edit_data($where, 'user')->result_array();
+
         $bahan = $this->bahan_model;
         $validation = $this->form_validation;
         $validation->set_rules($bahan->rules());
 
-        if ($validation->run()) {
+        if ($validation->run() == false) {
+        } else {
             $bahan->update();
             $this->session->set_flashdata('success', 'Berhasil disimpan');
+            // redirect('bahan');
         }
+        $this->load->view('templates/header', $data);
+        $this->load->view('templates/sidebar', $data);
+        $this->load->view('templates/topbar', $data);
+        $this->load->view('bahan/edit_form', $data);
+        $this->load->view('templates/footer');
 
         $data["bahan"] = $bahan->getById($id);
         if (!$data["bahan"]) show_404();
-
-        $this->load->view("bahan/edit_form", $data);
     }
 
     public function delete($id = null)
