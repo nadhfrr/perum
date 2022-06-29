f0310e34f5d8e555c719670530375dbfcad0e89f
yeunarnd <ynarnd23@gmail.com>
2022-06-30 00:44:52 +0700

    t

--- application/controllers/Keuangan.php
+++ application/controllers/Keuangan.php
@@ -11,7 +11,7 @@ class Keuangan extends CI_Controller
 
     public function index()
     {
-        $data['title'] = 'Dashboard';
+        $data['title'] = 'Utama';
         $data['user'] = $this->db->get_where('user', ['email' => $this->session->userdata('email')])->row_array();
 
         $this->load->view('templates/header', $data);
--- application/views/upah/edit_form.php
+++ application/views/upah/edit_form.php
@@ -19,76 +19,76 @@
 
 			<!-- Card  -->
 			<div class="card mb-3">
-					<div class="card-header">
+				<div class="card-header">
 
-						<a href="<?php echo site_url('upah') ?>"><i class="fas fa-arrow-left"></i>
-							Kembali</a>
-					</div>
-					<div class="card-body">
+					<a href="<?php echo site_url('upah') ?>"><i class="fas fa-arrow-left"></i>
+						Kembali</a>
+				</div>
+				<div class="card-body">
 
-						<form action="<?php base_url("upah/edit") ?>" method="post" enctype="multipart/form-data">
+					<form action="<?php base_url("upah/edit") ?>" method="post" enctype="multipart/form-data">
 
-							<input type="hidden" name="id" value="<?php echo $upah->kd_upah ?>" />
+						<!-- <input type="hidden" name="id" value="<?php echo $upah->kd_upah ?>" /> -->
 
-							<div class="form-group">
-								<label for="kd_upah">Kode Bahan</label>
-								<input class="form-control <?php echo form_error('kd_upah') ? 'is-invalid' : '' ?>" type="text" name="kd_upah" placeholder="Kode Upah" value="<?php echo $upah->kd_upah ?>" />
-								<div class="invalid-feedback">
-									<?php echo form_error('kd_upah') ?>
-								</div>
+						<div class="form-group">
+							<label for="kd_upah">Kode Bahan</label>
+							<input class="form-control <?php echo form_error('kd_upah') ? 'is-invalid' : '' ?>" type="text" name="kd_upah" placeholder="Kode Upah" value="<?php echo $upah->kd_upah ?>" />
+							<div class="invalid-feedback">
+								<?php echo form_error('kd_upah') ?>
 							</div>
+						</div>
 
-							<div class="form-group">
-								<label for="uraian_upah">Uraian Upah</label>
-								<input class="form-control <?php echo form_error('uraian_upah') ? 'is-invalid' : '' ?>" type="text" name="uraian_upah" min="0" placeholder="Uraian Upah" value="<?php echo $upah->uraian_upah ?>" />
-								<div class="invalid-feedback">
-									<?php echo form_error('uraian_upah') ?>
-								</div>
+						<div class="form-group">
+							<label for="uraian_upah">Uraian Upah</label>
+							<input class="form-control <?php echo form_error('uraian_upah') ? 'is-invalid' : '' ?>" type="text" name="uraian_upah" min="0" placeholder="Uraian Upah" value="<?php echo $upah->uraian_upah ?>" />
+							<div class="invalid-feedback">
+								<?php echo form_error('uraian_upah') ?>
 							</div>
+						</div>
 
-							<div class="form-group">
-								<label for="satuan">Satuan</label>
-								<input class="form-control <?php echo form_error('satuan') ? 'is-invalid' : '' ?>" type="text" name="satuan" min="0" placeholder="Satuan" value="<?php echo $upah->satuan ?>" />
-								<div class="invalid-feedback">
-									<?php echo form_error('satuan') ?>
-								</div>
+						<div class="form-group">
+							<label for="satuan">Satuan</label>
+							<input class="form-control <?php echo form_error('satuan') ? 'is-invalid' : '' ?>" type="text" name="satuan" min="0" placeholder="Satuan" value="<?php echo $upah->satuan ?>" />
+							<div class="invalid-feedback">
+								<?php echo form_error('satuan') ?>
 							</div>
+						</div>
 
-							<div class="form-group">
-								<label for="harga_dasar">Harga Dasar</label>
-								<input class="form-control <?php echo form_error('harga_dasar') ? 'is-invalid' : '' ?>" type="text" name="harga_dasar" min="0" placeholder="Harga Dasar" value="<?php echo $upah->harga_dasar ?>" />
-								<div class="invalid-feedback">
-									<?php echo form_error('harga_dasar') ?>
-								</div>
+						<div class="form-group">
+							<label for="harga_dasar">Harga Dasar</label>
+							<input class="form-control <?php echo form_error('harga_dasar') ? 'is-invalid' : '' ?>" type="text" name="harga_dasar" min="0" placeholder="Harga Dasar" value="<?php echo $upah->harga_dasar ?>" />
+							<div class="invalid-feedback">
+								<?php echo form_error('harga_dasar') ?>
 							</div>
+						</div>
 
-							<div class="form-group">
-								<label for="merk">Merk</label>
-								<input class="form-control <?php echo form_error('merk') ? 'is-invalid' : '' ?>" type="text" name="merk" min="0" placeholder="Merk" value="<?php echo $upah->merk ?>" />
-								<div class="invalid-feedback">
-									<?php echo form_error('merk') ?>
-								</div>
+						<div class="form-group">
+							<label for="merk">Merk</label>
+							<input class="form-control <?php echo form_error('merk') ? 'is-invalid' : '' ?>" type="text" name="merk" min="0" placeholder="Merk" value="<?php echo $upah->merk ?>" />
+							<div class="invalid-feedback">
+								<?php echo form_error('merk') ?>
 							</div>
+						</div>
 
-							<div class="form-group">
-								<label for="spesifikasi">Spesifikasi</label>
-								<input class="form-control <?php echo form_error('spesifikasi') ? 'is-invalid' : '' ?>" type="text" name="spesifikasi" min="0" placeholder="Spesifikasi" value="<?php echo $upah->spesifikasi ?>" />
-								<div class="invalid-feedback">
-									<?php echo form_error('spesifikasi') ?>
-								</div>
+						<div class="form-group">
+							<label for="spesifikasi">Spesifikasi</label>
+							<input class="form-control <?php echo form_error('spesifikasi') ? 'is-invalid' : '' ?>" type="text" name="spesifikasi" min="0" placeholder="Spesifikasi" value="<?php echo $upah->spesifikasi ?>" />
+							<div class="invalid-feedback">
+								<?php echo form_error('spesifikasi') ?>
 							</div>
+						</div>
 
-							<input class="btn btn-dark" type="submit" name="btn" value="Simpan" />
-						</form>
-
-					</div>
-
-					<div class="card-footer small text-muted">
-						*Wajib diisi
-					</div>
-
+						<input class="btn btn-dark" type="submit" name="btn" value="Simpan" />
+					</form>
 
 				</div>
+
+				<div class="card-footer small text-muted">
+					*Wajib diisi
+				</div>
+
+
+			</div>
 		</div>
 	</div>
 
