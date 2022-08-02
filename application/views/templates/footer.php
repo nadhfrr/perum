14b6a4e4d420b71c97c1fbb40aaf19d94b147b3a
yeunarnd <ynarnd23@gmail.com>
2022-08-02 23:04:10 +0700

    Update footer.php

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -85,6 +85,25 @@
   }
 </script>
 
+<script>
+  $('.form-check-input').on('click', function() {
+    const menuId = $(this).data('menu');
+    const roleId = $(this).data('role');
+
+    $.ajax({
+      url: "<?= base_url('admin/changeaccess'); ?>",
+      type: 'post',
+      data: {
+        menuId: menuId,
+        roleId: roleId
+      },
+      success: function() {
+        document.location.href = "<?= base_url('admin/roleaccess/'); ?>" + roleId;
+      }
+    })
+  });
+</script>
+
 </body>
 
 </html>
