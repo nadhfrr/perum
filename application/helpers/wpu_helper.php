f0a7daef4fe8c634bf3695cd19223e66643c2660
yeunarnd <ynarnd23@gmail.com>
2022-08-02 22:14:15 +0700

    Update wpu_helper.php

--- application/helpers/wpu_helper.php
+++ application/helpers/wpu_helper.php
@@ -17,7 +17,7 @@ function is_logged_in()
             'menu_id' => $menu_id
         ]);
 
-        if ($userAccess->num_rows() > 0) {
+        if ($userAccess->num_rows() < 1) {
             redirect('auth/blocked');
         }
     }
@@ -33,5 +33,26 @@ function check_access($role_id, $menu_id)
 
     if ($result->num_rows() > 0) {
         return "checked='checked'";
+    }
+}
+
+if (!function_exists('format_indo')) {
+    function format_indo($date)
+    {
+        date_default_timezone_set('Asia/Jakarta');
+        // array hari dan bulan
+        // $Hari = array("Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu");
+        $Bulan = array("Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember");
+
+        // pemisahan tahun, bulan, hari, dan waktu
+        $tahun = substr($date, 0, 4);
+        $bulan = substr($date, 5, 2);
+        $tgl = substr($date, 8, 2);
+        $waktu = substr($date, 11, 5);
+        $hari = date("w", strtotime($date));
+        // $result = $Hari[$hari] . ", " . $tgl . " " . $Bulan[(int)$bulan - 1] . " " . $tahun . " " . $waktu;
+        $result = $tgl . " " . $Bulan[(int)$bulan - 1] . " " . $tahun . " " . $waktu;
+
+        return $result;
     }
 }
