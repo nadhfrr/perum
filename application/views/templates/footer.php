6732bb97bdab2db6507e5af14f52ea5d79e6175a
yeunarnd <ynarnd23@gmail.com>
2022-08-08 23:23:02 +0700

    y

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -63,16 +63,13 @@
 <!-- Core plugin JavaScript-->
 <script src="<?= base_url('assets2/'); ?>assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
+<!-- Page level plugin JavaScript-->
+<script src="<?= base_url('assets2/'); ?>vendor/chart.js/Chart.min.js"></script>
+<script src="<?= base_url('assets2/'); ?>vendor/datatables/jquery.dataTables.js"></script>
+<script src="<?= base_url('assets2/'); ?>vendor/datatables/dataTables.bootstrap4.js"></script>
+
 <!-- Custom scripts for all pages-->
 <script src="<?= base_url('assets2/'); ?>assets/js/sb-admin-2.min.js"></script>
-
-<!-- Page level plugin JavaScript-->
-<script src="<?= base_url('assets2/vendor/chart.js/Chart.min.js'); ?>"></script>
-<script src="<?= base_url('assets2/vendor/datatables/jquery.dataTables.js'); ?>"></script>
-<script src="<?= base_url('assets2/vendor/datatables/dataTables.bootstrap4.js'); ?>"></script>
-
-<!-- Custom scripts for all pages-->
-<script src="<?= base_url('assets2/'); ?>js/sb-admin-2.min.js"></script>
 
 <!-- Demo scripts for this page-->
 <script src="<?= base_url('assets2/js/demo/datatables-demo.js'); ?>"></script>
