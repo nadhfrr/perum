a830da1940bf3ddbd991c82de0504263c7d3a581
yeunarnd <ynarnd23@gmail.com>
2022-08-27 22:45:22 +0700

    Update role-access.php

--- application/views/admin/role-access.php
+++ application/views/admin/role-access.php
@@ -2,39 +2,48 @@
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <h1 class="h3 mb-4 text-gray-800"><?= $title; ?></h1>
+    <h1 class="h3 mb-4 text-gray-800">Role : <?= $role['role']; ?></h1>
 
     <div class="row">
-        <div class="col-lg-6">
+        <div class="col-lg">
 
             <?= $this->session->flashdata('message'); ?>
 
-            <h5>Role : <?= $role['role']; ?></h5>
+            <!-- DataTables -->
+            <div class="card mb-3">
+                <div class="card-header">
+                    <a href="<?php echo site_url('admin/role') ?>"><i class="fas fa-arrow-left"></i> Kembali</a>
+                </div>
 
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Menu</th>
-                        <th scope="col">Akses</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php $i = 1; ?>
-                    <?php foreach ($menu as $m) : ?>
-                        <tr>
-                            <th scope="row"><?= $i; ?></th>
-                            <td><?= $m['menu']; ?></td>
-                            <td>
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" <?= check_access($role['id'], $m['id']); ?> data-role="<?= $role['id']; ?>" data-menu="<?= $m['id']; ?>">
-                                </div>
-                            </td>
-                        </tr>
-                        <?php $i++; ?>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+                <div class="card-body">
+                    <div class="table-responsive">
+                        <table class="table table-hover" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Menu</th>
+                                    <th scope="col">Access</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php $i = 1; ?>
+                                <?php foreach ($menu as $mn) : ?>
+                                    <tr>
+                                        <th scope="row"><?= $i; ?></th>
+                                        <td><?= $mn['menu']; ?></td>
+                                        <td>
+                                            <div class="form-check">
+                                                <input class="form-check-input" type="checkbox" <?= check_access($role['id'], $mn['id']); ?> data-role="<?= $role['id']; ?>" data-menu="<?= $mn['id']; ?>">
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <?php $i++; ?>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
